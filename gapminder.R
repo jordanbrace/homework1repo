aa12e2a33c459ed1fef8e9596caa59b5bf1c072c
Jordan Brace <jordan_brace@yahoo.ca>
2014-09-15 10:53:38 -0700

    pick up at barplot()

--- gapminder.R
+++ gapminder.R
@@ -10,3 +10,21 @@ ncol(gDat)
 length(gDat)
 summary(gDat)
 unique(gDat$country)
+
+plot(lifeExp~year, gDat) #good
+plot(gDat[,5]~gDat[,2]) #bad programming style
+plot(lifeExp~gdpPercap, gDat)
+
+library(ggplot2)
+
+gDat$lifeExp #press tab after dollar sign for list of variables
+summary(gDat$lifeExp)
+str(gDat$lifeExp)
+hist(gDat$lifeExp) #tab after a character after $ will autocomplete (or list variables that start that way)
+
+gDat$continent
+class(gDat$continent)
+str(gDat$continent) #note, factors storing info as integers and not character!! (not sure when thatll screw stuff up but its definitely ever)
+levels(gDat$continent)
+table(gDat$continent)
+barplot(table(gDat$continent))
